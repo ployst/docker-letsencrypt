b5307ad2d40aefe6b997aedfbbea6257cc445b7e
Ankur Maharjan <akr.mhrjn@gmail.com>
2016-12-14 11:52:42 +0545

    Update save_certs.sh

--- save_certs.sh
+++ save_certs.sh
@@ -26,6 +26,7 @@ KEY=$(cat $CERT_LOCATION/$DOMAIN/privkey.pem | base64 --wrap=0)
 DHPARAM=$(openssl dhparam 2048 | base64 --wrap=0)
 
 NAMESPACE=${NAMESPACE:-default}
+TYPE=${TYPE:-Opaque}
 
 kubectl get secrets --namespace $NAMESPACE $SECRET_NAME && ACTION=replace || ACTION=create;
 
